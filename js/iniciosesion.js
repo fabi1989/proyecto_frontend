ac9e82d89661d07c15cf3b3b84b9015d1be43128
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-12-02 18:06:51 +0100

    RegEx iniciosesion

--- js/iniciosesion.js
+++ js/iniciosesion.js
@@ -1,12 +1,12 @@
 function validar(iniciosesion) {
-  if (iniciosesion.email.value.trim().length == 0) {
-    alert("correo obligatorio");
-    return false;
+  if (/^\w+[a-z]i([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)) {
+    alert("la dirección de email" + valor + "es correcta.");
+  } else {
+    alert("la dirección de email es incorrecta");
   }
 
-
   if (iniciosesion.contraseña.value.trim().length == 0) {
-    alert("contraseña obligatoria");
+    alert("Contraseña obligatoria");
     return false;
   }
 
